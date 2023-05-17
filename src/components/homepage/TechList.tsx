80378e5656d2191ea810d59f9ca460480b058767
Daniela Vornic <daniela.vornic@gmail.com>
2023-05-17 13:16:42 +0300

    Update TechList.tsx

--- src/components/homepage/TechList.tsx
+++ src/components/homepage/TechList.tsx
@@ -2,26 +2,19 @@ const skills = ["React", "Next.js", "TypeScript", "Tailwind CSS", "JavaScript", 
 const firstColumnSkills = skills.slice(0, 3);
 const secondColumnSkills = skills.slice(3);
 
-const SkillItem = ({ skill }: { skill: string }) => (
-  <div className="flex items-center space-x-2">
-    <div className="w-1 h-1 rounded-full bg-lavender" />
-    <span className="text-subtext0">{skill}</span>
-  </div>
-);
-
 const TechList = () => {
   return (
-    <div className="flex flex-col md:flex-row md:space-x-8">
-      <div className="flex flex-col space-y-3 mb-3 md:mb-0">
+    <div className="flex flex-col text-subtext0 md:flex-row md:space-x-8">
+      <ul className="list-disc list-inside marker:text-lavender space-y-3 mb-3 md:mb-0">
         {firstColumnSkills.map((skill) => (
-          <SkillItem key={skill} skill={skill} />
+          <li key={skill}>{skill}</li>
         ))}
-      </div>
-      <div className="flex flex-col space-y-3">
+      </ul>
+      <ul className="list-disc list-inside marker:text-lavender space-y-3">
         {secondColumnSkills.map((skill) => (
-          <SkillItem key={skill} skill={skill} />
+          <li key={skill}>{skill}</li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
